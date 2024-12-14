2cb01d1464cd2ff325d181fb2550999bf4f800c2
MatanShechori <smatan@campus.technion.ac.il>
2024-12-14 16:51:19 +0200

    initial commit

---
+++ HW1.m
@@ -0,0 +1,34 @@
+%% Known Var of the Question
+
+m = 1; % kg
+g = 9.81; %m/sec^2
+L0 = 0.2; %m
+a = 1; % m
+k = 4; %N/m
+c1 = 0.1;
+c3 = 0.001;
+teta = 30; %degrees
+
+%% quesion 5
+
+
+
+%% question 6
+
+
+
+%% question 7
+
+%% Q8
+
+
+%% Q9
+
+
+%% Q10
+
+%% Q11
+
+
+%% Q12
+
