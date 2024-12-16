4a7d7422e74cd914506bf4dc3c7f9cf92ef99a11
MatanShechori <matan@airev.aero>
2024-12-16 10:55:34 +0200

    q5 updated simpler

--- HW1.m
+++ HW1.m
@@ -7,17 +7,25 @@ a = 1; % m
 k = 4; %N/m
 c1 = 0.1;
 c3 = 0.001;
-teta = 30; %degrees
+teta = 30*pi/180; % rad
 
 %% quesion 5
 
+f = @(x) k*(1- L0/(sqrt(x^2+a^2)))*x-m*g*sin(teta);
+
+root  = fzero(f,1);
+fprintf('root' , root);
 
 
 %% question 6
+L0 = 5; %m new initial length
+sol = fzero(f,1);
 
-
+disp(sol);
 
 %% question 7
+
+
 
 %% Q8
 
