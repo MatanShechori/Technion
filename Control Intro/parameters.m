4503ab8a5f2ff77eb3cefc851e22d020ae1cb24a
MatanShechori <matan@airev.aero>
2025-01-08 13:09:06 +0200

    initial commit project

---
+++ Control Intro/parameters.m
@@ -0,0 +1,5 @@
+km = 0.0242; % Nm/A
+Ra = 2.15; % Ohm
+J = 0.0047; % kgm^2
+f = 0.004; % Nms/rad
+ng = 4.85; % gear ratio
---
+++ Control Intro/project.m
@@ -0,0 +1,8 @@
+%% Question 4
+
+%% Question 4a
+
+% Define the parameters of the system
+run('parameters.m');
+
+
