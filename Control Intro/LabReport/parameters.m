d2a81379f308c1c2db7080af5ded6d3a546997c8
MatanShechori <matan@airev.aero>
2025-02-01 11:29:10 +0200

    simulink simulation

--- Control Intro/LabReport/parameters.m
+++ Control Intro/LabReport/parameters.m
@@ -1,7 +1,7 @@
-km = 0.0242; % Nm/A
-Ra = 2.15; % Ohm
+km = 0.0243; % Nm/A
+Ra = 2.08; % Ohm
 J = 0.0047; % kgm^2
-f = 0.004; % Nms/rad
-ng = 4.85; % gear ratio
+f = 0.0077* (pi/180); % Nms/Deg
+ng = 5.25; % gear ratio
 s = tf('s');
-P = km*ng/(Ra*J*s^2+Ra*f*s+ng^2*km^2*s);
+P = km*ng/s*(Ra*J*s+Ra*f+ng^2*km^2);
