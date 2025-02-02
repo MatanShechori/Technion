7cd09088f154df8694cdddfdd753cac9e5c2571d
MatanShechori <matan@airev.aero>
2025-02-02 20:24:38 +0200

    control lab updates

--- Control Intro/LabReport/ControlLab.m
+++ Control Intro/LabReport/ControlLab.m
@@ -1,48 +1,40 @@
 %% Intro to Conrol lap report
 % This report is based on the project and parameters.m
 
-run parameters.m
-kp = 6;
-
-% Simulate step response
-t = 0:0.01:15; % Time vector from 0 to 5 seconds
-[y, t] = step(kp*P, t);
-figure;
-plot(t,y);
-
-% Plot the step response
-figure;
-plot(t, y, 'LineWidth', 1.5);
-grid on;
-title('Step Response with Kp = 0.0485');
-xlabel('Time [s])');
-ylabel('Amplitude');
 
 matlist12 = {'q12 0.1', 'q12 0.2', 'q12 0.3', 'q12 0.05', 'q12 0.36'};
+simmatlist12 = {'q12 0.1 sim', 'q12 0.2 sim', 'q12 0.3 sim', 'q12 005 sim', 'q12 0.36 sim'};
+simmatlist18 = {'q18 0.1 sim', 'q18 0.4 sim', 'q18 0.8 sim', 'q18 1.1 sim'};
 matlist18 = {'q18 0.1','q18 0.4','q18 0.8',  'q18 1.1'};
 for i= 1:length(matlist12)
     
     load(matlist12{i});
+    load(simmatlist12{i});
     figure;
-    
-    plot(u.time,u.signals.values);
+    plot(out.y_sim.Time,out.y_sim.Data);
     hold on;
     plot(y.time,y.signals.values);
+    yline(30, '--r', 'Reference'); % Add horizontal line at y=30
     hold off;
     title(matlist12{i});
     xlabel('Time [s]');
     ylabel('Amplitude');
+    legend('Simulation','Experiment', 'Reference');
+    grid on;
 end
+% plotting the step response of the system for PI controller
 for i= 1:length(matlist18)
     load(matlist18{i});
     figure;
-    plot(u.time,u.signals.values);
+    plot(out.y_sim.Time,out.y_sim.Data);
     hold on;
     plot(y.time,y.signals.values);
+    yline(30, '--r', 'Reference'); % Add horizontal line at y=30
     hold off;
     title(matlist18{i});
     xlabel('Time [s]');
     ylabel('Amplitude');
+    legend('Input', 'Output', 'Reference');
 end
 
 sisotool(kp*P);
